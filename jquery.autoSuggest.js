971012d3e09f5836331db6a38f3a13355770572b
Derrick Camerino <robustdj@gmail.com>
2011-05-28 21:04:04 -0700

    adding name and new value support

--- jquery.autoSuggest.js
+++ jquery.autoSuggest.js
@@ -19,10 +19,13 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 
+
 (function($){
 	$.fn.autoSuggest = function(data, options) {
-		var defaults = { 
+		var defaults = {
 			asHtmlID: false,
+			asHtmlName: false,
+			newValuesInputName: false,
 			startText: "Enter Name Here",
 			emptyText: "No Results Found",
 			preFill: {},
@@ -49,9 +52,9 @@
 		  	retrieveComplete: function(data){ return data; },
 		  	resultClick: function(data){},
 		  	resultsComplete: function(){}
-	  	};  
-	 	var opts = $.extend(defaults, options);	 	
-		
+	  	};
+	 	var opts = $.extend(defaults, options);
+
 		var d_type = "object";
 		var d_count = 0;
 		if(typeof data == "string") {
@@ -66,31 +69,38 @@
 				if(!opts.asHtmlID){
 					x = x+""+Math.floor(Math.random()*100); //this ensures there will be unique IDs on the page if autoSuggest() is called multiple times
 					var x_id = "as-input-"+x;
+          var as_values_name = "as_values_"+x;
 				} else {
 					x = opts.asHtmlID;
 					var x_id = x;
+          var as_values_name = "as_values_"+x;
 				}
+
+        if(opts.asHtmlName){
+          var as_values_name = opts.asHtmlName;
+        }
 				opts.start.call(this);
 				var input = $(this);
 				input.attr("autocomplete","off").addClass("as-input").attr("id",x_id).val(opts.startText);
 				var input_focus = false;
-				
+
 				// Setup basic elements and render them to the DOM
 				input.wrap('<ul class="as-selections" id="as-selections-'+x+'"></ul>').wrap('<li class="as-original" id="as-original-'+x+'"></li>');
+        var count_of_new = 0;
 				var selections_holder = $("#as-selections-"+x);
-				var org_li = $("#as-original-"+x);				
+				var org_li = $("#as-original-"+x);
 				var results_holder = $('<div class="as-results" id="as-results-'+x+'"></div>').hide();
 				var results_ul =  $('<ul class="as-list"></ul>');
-				var values_input = $('<input type="hidden" class="as-values" name="as_values_'+x+'" id="as-values-'+x+'" />');
+				var values_input = $('<input type="hidden" class="as-values" name="'+as_values_name+'" id="as-values-'+x+'" />');
 				var prefill_value = "";
 				if(typeof opts.preFill == "string"){
-					var vals = opts.preFill.split(",");					
+					var vals = opts.preFill.split(",");
 					for(var i=0; i < vals.length; i++){
 						var v_data = {};
 						v_data[opts.selectedValuesProp] = vals[i];
 						if(vals[i] != ""){
-							add_selected_item(v_data, "000"+i);	
-						}		
+							add_selected_item(v_data, "000"+i);
+						}
 					}
 					prefill_value = opts.preFill;
 				} else {
@@ -103,8 +113,8 @@
 							if(new_v == undefined){ new_v = ""; }
 							prefill_value = prefill_value+new_v+",";
 							if(new_v != ""){
-								add_selected_item(opts.preFill[i], "000"+i);	
-							}		
+								add_selected_item(opts.preFill[i], "000"+i);
+							}
 						}
 					}
 				}
@@ -119,15 +129,15 @@
 				selections_holder.click(function(){
 					input_focus = true;
 					input.focus();
-				}).mousedown(function(){ input_focus = false; }).after(results_holder);	
+				}).mousedown(function(){ input_focus = false; }).after(results_holder);
 
 				var timeout = null;
 				var prev = "";
 				var totalSelections = 0;
 				var tab_press = false;
-				
+
 				// Handle input field events
-				input.focus(function(){			
+				input.focus(function(){
 					if($(this).val() == opts.startText && values_input.val() == ""){
 						$(this).val("");
 					} else if(input_focus){
@@ -145,7 +155,7 @@
 					} else if(input_focus){
 						$("li.as-selection-item", selections_holder).addClass("blur").removeClass("selected");
 						results_holder.hide();
-					}				
+					}
 				}).keydown(function(e) {
 					// track last key pressed
 					lastKeyPressCode = e.keyCode;
@@ -160,7 +170,7 @@
 							moveSelection("down");
 							break;
 						case 8:  // delete
-							if(input.val() == ""){							
+							if(input.val() == ""){
 								var last = values_input.val().split(",");
 								last = last[last.length - 2];
 								selections_holder.children().not(org_li.prev()).removeClass("selected");
@@ -169,7 +179,7 @@
 									opts.selectionRemoved.call(this, org_li.prev());
 								} else {
 									opts.selectionClick.call(this, org_li.prev());
-									org_li.prev().addClass("selected");		
+									org_li.prev().addClass("selected");
 								}
 							}
 							if(input.val().length == 1){
@@ -181,18 +191,6 @@
 								timeout = setTimeout(function(){ keyChange(); }, opts.keyDelay);
 							}
 							break;
-						case 9: case 188:  // tab or comma
-							tab_press = true;
-							var i_input = input.val().replace(/(,)/g, "");
-							if(i_input != "" && values_input.val().search(","+i_input+",") < 0 && i_input.length >= opts.minChars){	
-								e.preventDefault();
-								var n_data = {};
-								n_data[opts.selectedItemProp] = i_input;
-								n_data[opts.selectedValuesProp] = i_input;																				
-								var lis = $("li", selections_holder).length;
-								add_selected_item(n_data, "00"+(lis+1));
-								input.val("");
-							}
 						case 13: // return
 							tab_press = false;
 							var active = $("li.active:first", results_holder);
@@ -207,7 +205,11 @@
 						default:
 							if(opts.showResultList){
 								if(opts.selectionLimit && $("li.as-selection-item", selections_holder).length >= opts.selectionLimit){
-									results_ul.html('<li class="as-message">'+opts.limitText+'</li>');
+                  updated_results_ul = results_ul.html('<li class="as-message">'+opts.limitText+'</li>');
+                  if (results_holder.children().length == 0){
+                    results_holder.append(updated_results_ul);
+                    results_ul.css("width", selections_holder.outerWidth());
+                  }
 									results_holder.show();
 								} else {
 									if (timeout){ clearTimeout(timeout); }
@@ -217,52 +219,54 @@
 							break;
 					}
 				});
-				
+
 				function keyChange() {
 					// ignore if the following keys are pressed: [del] [shift] [capslock]
 					if( lastKeyPressCode == 46 || (lastKeyPressCode > 8 && lastKeyPressCode < 32) ){ return results_holder.hide(); }
+          if(opts.selectionLimit && $("li.as-selection-item", selections_holder).length >= opts.selectionLimit){ return; }
 					var string = input.val().replace(/[\\]+|[\/]+/g,"");
 					if (string == prev) return;
 					prev = string;
-					if (string.length >= opts.minChars) {
-						selections_holder.addClass("loading");
-						if(d_type == "string"){
-							var limit = "";
-							if(opts.retrieveLimit){
-								limit = "&limit="+encodeURIComponent(opts.retrieveLimit);
-							}
-							if(opts.beforeRetrieve){
-								string = opts.beforeRetrieve.call(this, string);
-							}
-							$.getJSON(req_string+"?"+opts.queryParam+"="+encodeURIComponent(string)+limit+opts.extraParams, function(data){ 
-								d_count = 0;
-								var new_data = opts.retrieveComplete.call(this, data);
-								for (k in new_data) if (new_data.hasOwnProperty(k)) d_count++;
-								processData(new_data, string); 
-							});
-						} else {
-							if(opts.beforeRetrieve){
-								string = opts.beforeRetrieve.call(this, string);
-							}
-							processData(org_data, string);
-						}
-					} else {
-						selections_holder.removeClass("loading");
-						results_holder.hide();
-					}
-				}
+          if (string.length >= opts.minChars) {
+            selections_holder.addClass("loading");
+            if(d_type == "string"){
+              var limit = "";
+              if(opts.retrieveLimit){
+                limit = "&limit="+encodeURIComponent(opts.retrieveLimit);
+              }
+              if(opts.beforeRetrieve){
+                string = opts.beforeRetrieve.call(this, string);
+              }
+              $.getJSON(req_string+"?"+opts.queryParam+"="+encodeURIComponent(string)+limit+opts.extraParams, function(data){
+                d_count = 0;
+                var new_data = opts.retrieveComplete.call(this, data);
+                for (k in new_data) if (new_data.hasOwnProperty(k)) d_count++;
+                processData(new_data, string);
+              });
+            } else {
+              if(opts.beforeRetrieve){
+                string = opts.beforeRetrieve.call(this, string);
+              }
+              processData(org_data, string);
+            }
+          } else {
+            selections_holder.removeClass("loading");
+            results_holder.hide();
+          }
+        }
 				var num_count = 0;
 				function processData(data, query){
+          var case_sensitive_query = query;
 					if (!opts.matchCase){ query = query.toLowerCase(); }
 					var matchCount = 0;
 					results_holder.html(results_ul.html("")).hide();
-					for(var i=0;i<d_count;i++){				
+					for(var i=0;i<d_count;i++){
 						var num = i;
 						num_count++;
 						var forward = false;
 						if(opts.searchObjProps == "value") {
 							var str = data[num].value;
-						} else {	
+						} else {
 							var str = "";
 							var names = opts.searchObjProps.split(",");
 							for(var y=0;y<names.length;y++){
@@ -271,12 +275,12 @@
 							}
 						}
 						if(str){
-							if (!opts.matchCase){ str = str.toLowerCase(); }				
+							if (!opts.matchCase){ str = str.toLowerCase(); }
 							if(str.search(query) != -1 && values_input.val().search(","+data[num][opts.selectedValuesProp]+",") == -1){
 								forward = true;
-							}	
+							}
 						}
-						if(forward){
+						if('true'){
 							var formatted = $('<li class="as-result-item" id="as-result-item-'+num+'"></li>').click(function(){
 									var raw_data = $(this).data("data");
 									var number = raw_data.num;
@@ -294,19 +298,19 @@
 									$(this).addClass("active");
 								}).data("data",{attributes: data[num], num: num_count});
 							var this_data = $.extend({},data[num]);
-							if (!opts.matchCase){ 
+							if (!opts.matchCase){
 								var regx = new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + query + ")(?![^<>]*>)(?![^&;]+;)", "gi");
 							} else {
 								var regx = new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + query + ")(?![^<>]*>)(?![^&;]+;)", "g");
 							}
-							
+
 							if(opts.resultsHighlight){
 								this_data[opts.selectedItemProp] = this_data[opts.selectedItemProp].replace(regx,"<em>$1</em>");
 							}
 							if(!opts.formatList){
 								formatted = formatted.html(this_data[opts.selectedItemProp]);
 							} else {
-								formatted = opts.formatList.call(this, this_data, formatted);	
+								formatted = opts.formatList.call(this, this_data, formatted);
 							}
 							results_ul.append(formatted);
 							delete this_data;
@@ -316,31 +320,58 @@
 					}
 					selections_holder.removeClass("loading");
 					if(matchCount <= 0){
-						results_ul.html('<li class="as-message">'+opts.emptyText+'</li>');
+            var formatted = $('<li class="as-result-item" id="as-result-item-1"></li>');
+            if(opts.newValuesInputName){
+              count_of_new += 1;
+              formatted.html('Add new: <em>' + case_sensitive_query + '</em>').click(function(){
+                input_focus = false;
+                new_data_item = {};
+                new_data_item[opts.selectedItemProp] = case_sensitive_query;
+                new_data_item[opts.selectedValuesProp] = case_sensitive_query;
+                add_selected_item(new_data_item, count_of_new, true);
+                input.val("");
+                results_holder.hide();
+              }).mousedown(function(){ input_focus = false; }).mouseover(function(){
+                $("li", results_ul).removeClass("active");
+                $(this).addClass("active");
+              });
+            } else {
+              formatted.html('<em>New items not allowed in this field</em>');
+            }
+						results_ul.append('<li class="as-message">'+opts.emptyText+'</li>').append(formatted);
 					}
 					results_ul.css("width", selections_holder.outerWidth());
 					results_holder.show();
 					opts.resultsComplete.call(this);
 				}
-				
-				function add_selected_item(data, num){
-					values_input.val(values_input.val()+data[opts.selectedValuesProp]+",");
+
+				function add_selected_item(data, num, is_new_value){
+          is_new_value = is_new_value || false;
+          if(!is_new_value){
+            values_input.val(values_input.val()+data[opts.selectedValuesProp]+",");
+          } else {
+            values_input.after('<input type="hidden" name="'+opts.newValuesInputName+'" value="'+data[opts.selectedValuesProp]+'">');
+          }
 					var item = $('<li class="as-selection-item" id="as-selection-'+num+'"></li>').click(function(){
 							opts.selectionClick.call(this, $(this));
 							selections_holder.children().removeClass("selected");
 							$(this).addClass("selected");
 						}).mousedown(function(){ input_focus = false; });
 					var close = $('<a class="as-close">&times;</a>').click(function(){
-							values_input.val(values_input.val().replace(","+data[opts.selectedValuesProp]+",",","));
+              if(!is_new_value){
+                values_input.val(values_input.val().replace(","+data[opts.selectedValuesProp]+",",","));
+              } else {
+                $('input[value='+data[opts.selectedValuesProp]+']').remove();
+              }
 							opts.selectionRemoved.call(this, item);
 							input_focus = true;
 							input.focus();
 							return false;
 						});
 					org_li.before(item.html(data[opts.selectedItemProp]).prepend(close));
-					opts.selectionAdded.call(this, org_li.prev());	
+					opts.selectionAdded.call(this, org_li.prev());
 				}
-				
+
 				function moveSelection(direction){
 					if($(":visible",results_holder).length > 0){
 						var lis = $("li", results_holder);
@@ -348,21 +379,21 @@
 							var start = lis.eq(0);
 						} else {
 							var start = lis.filter(":last");
-						}					
+						}
 						var active = $("li.active:first", results_holder);
 						if(active.length > 0){
 							if(direction == "down"){
 							start = active.next();
 							} else {
 								start = active.prev();
-							}	
+							}
 						}
 						lis.removeClass("active");
 						start.addClass("active");
 					}
 				}
-									
+
 			});
 		}
 	}
-})(jQuery);  	
+})(jQuery);
